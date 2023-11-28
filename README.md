b0030941e2f6ea2db041db68ce072ba85498937c
overcl0ck0 <147282609+overcl0ck0@users.noreply.github.com>
2023-11-28 06:45:31 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+Overclock_Attack
---
+++ requirement.sh
@@ -0,0 +1,25 @@
+#!/bin/python
+# 
+#
+# Name	:	IP Attack
+# Edit	: 	OverClock92
+#
+#	<==(__WARNING__)==>
+#
+# DO NOT MODIFY OR COPY WITHOUT PERMISSION !!!
+#
+#	<==(__WARNING__)==>
+#
+#
+#
+apt update
+apt upgrade 
+apt install figlet -y
+apt install toilet -y
+apt install nano -y
+apt install ruby -y
+apt install curl -y
+gem install lolcat
+clear
+toilet -f pagga "Done..!!" | lolcat
+
